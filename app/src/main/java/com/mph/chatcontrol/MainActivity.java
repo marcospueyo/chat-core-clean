9021c701ac95a2705ded2171b73b31199f1c6d76
Marcos Pueyo <marcosph0@gmail.com>
2017-07-12 18:07:17 +0200

    - Added bottom navigation view

--- app/src/main/java/com/mph/chatcontrol/MainActivity.java
+++ app/src/main/java/com/mph/chatcontrol/MainActivity.java
@@ -1,13 +1,34 @@
 package com.mph.chatcontrol;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @BindView(R.id.bottom_nav_view)
+    BottomNavigationView mBottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mBottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Log.d(TAG, "onNavigationItemSelected: itemID=" + item.getItemId());
+                return true;
+            }
+        });
     }
 }
